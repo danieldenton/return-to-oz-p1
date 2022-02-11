3f2407618c3ee3e346ac5fa8693ab3a666c4cf14
danieldenton <danielmdenton@gmail.com>
2022-02-10 16:04:21 -0800

    final deploy

--- js3.js
+++ js3.js
@@ -10,19 +10,35 @@ const ornanments = [
   document.getElementById("object9"),
 ];
 
+//variables for winning and incorrectselctions that change everytime the game is played
 const [winningSelections, incorrectSelections] = randomSplit(ornanments, 3);
-const playerWinSel = [];
-const playerInc = [];
+// const playerWinSel = [];
+// const playerInc = [];
+
+// function to shuffle the array of ornaments
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
+// function that calls the shuffle and separates the shuffled array into 2
+function randomSplit(arr, idx) {
+  shuffle(arr);
+  const start = arr.slice(0, idx);
+  const end = arr.slice(idx, arr.length);
+  return [start, end];
+}
 
 let correctClicked = 0;
 let incorrectClicked = 0;
-function totalClicks() {
-  return correctClicked + incorrectClicked;
-}
 
 const audio1 = new Audio("audio/correct.wav");
-const audio2 = new Audio("audio/wrong.wav");
-
+const audio2 = new Audio("audio/incorrect.wav");
+const audio3 = new Audio("audio/laugh.wav");
+// The click that adds to correctClicked, plays sound, changes the images and the text on the screen
 winningSelections.forEach(function (orn) {
   orn.addEventListener(
     "click",
@@ -43,74 +59,72 @@ winningSelections.forEach(function (orn) {
       document.getElementById("text").innerText =
         textResponses[correctClicked - 1];
       document.getElementById("text").style.color = "rgb(170, 255, 0)";
+      // if 3 correct selections were made the game is over and display result is called
       if (correctClicked === 3) {
-        setInterval(function () {
+        setTimeout(function () {
           displayResult();
         }, 3000);
       }
     },
     {
+      // so you can't click on the winning boxes more thamn once
       once: true,
     }
   );
 });
-
+// same as above for incorrectSelections
 incorrectSelections.forEach(function (orn) {
   orn.addEventListener("click", function () {
     incorrectClicked += 1;
+    // instead of changing the images the selections disappear
     orn.style.display = "none";
     audio2.play();
     const textResponses = [
       "That was not one of your friends!",
       "You're running out of chances!",
       "Now you're down to your last chance!",
-      "",
     ];
     document.getElementById("text").innerText =
       textResponses[incorrectClicked - 1];
     document.getElementById("text").style.color = "rgb(240, 225, 60)";
+    // if 4 incorrect selections are made the game is over
     if (incorrectClicked === 4) {
       displayResult();
     }
   });
 });
 
-function shuffle(a) {
-  for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [a[i], a[j]] = [a[j], a[i]];
-  }
-  return a;
-}
-
-function randomSplit(arr, idx) {
-  shuffle(arr);
-  const start = arr.slice(0, idx);
-  const end = arr.slice(idx, arr.length);
-  return [start, end];
+function endLaugh() {
+  setTimeout(function () {
+    audio3.play();
+  }, 1000);
 }
 
 function displayResult() {
   document.querySelector("#container").style.display = "none";
   document.querySelector("#background-text").innerText = "";
   if (correctClicked === 1) {
+    endLaugh();
     document.querySelector("#result").innerText =
-      "Dorothy managed to save Scarecrow, but Jack, Tik-Tok and the rest of Oz still belong to the Gnome King.";
+      "Dorothy managed to save Scarecrow, but Jack, TikTok and the rest of Oz still belong to the Gnome King.";
     document.querySelector("body").style.backgroundImage = "url(images/GK.png)";
     document.querySelector("#play-again").innerText = "Play again";
   } else if (correctClicked === 2) {
+    endLaugh();
     document.querySelector("#result").innerText =
-      "Dorothy managed to save Scarecrow and Jack, but Tik-Tok and the rest of Oz still belong to the Gnome King.";
+      "Dorothy managed to save Scarecrow and Jack, but TikTok and the rest of Oz still belong to the Gnome King.";
     document.querySelector("body").style.backgroundImage = "url(images/GK.png)";
     document.querySelector("#play-again").innerText = "Play again";
     ("href=beware.png display-text");
-  } else if (incorrectClicked === 3) {
+  } else if (correctClicked === 3) {
+    // because of some overlapping divs near the envent listeners this ending calls a new HTML doc
+    window.location.href = "index4.html";
+  } else {
+    endLaugh();
     document.querySelector("#result").innerText =
       "Dorothy has failed to save her friends. They and the rest of Oz still belong to the Gnome King.";
     document.querySelector("body").style.backgroundImage = "url(images/GK.png)";
     document.querySelector("#play-again").innerText = "Play again";
-  } else {
-    window.location.href = "index4.html";
   }
 }
 
